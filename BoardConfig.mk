03d3263cccbd3101d103554f97fbe6803b9dd9cb
dianlujitao <dianlujitao@gmail.com>
2016-06-15 19:05:17 +0800

    oneplus3: Use 64-bit binder
    Change-Id: I7853dd5cc3a747c1bee8496909aa4e0d31db693e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,8 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+TARGET_USES_64_BIT_BINDER := true
+
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-uart"
 
