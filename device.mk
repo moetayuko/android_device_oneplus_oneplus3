357e56fe1725b7264a95da82792489fd52533b54
dianlujitao <dianlujitao@gmail.com>
2016-06-17 23:39:20 +0800

    oneplus3: Copy missing audio_policy.conf
    Change-Id: Icd7fd9fb98d632846ee7aa85ed477fc79bdf599c

--- device.mk
+++ device.mk
@@ -80,6 +80,7 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration file
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
