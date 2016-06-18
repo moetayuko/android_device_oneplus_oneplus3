5efc7c509d1ecd1a8515fa7e31bfe801814a6601
dianlujitao <dianlujitao@gmail.com>
2016-06-18 15:37:02 +0800

    oneplus3: Fix a typo
     * Oops!!!
    
    Change-Id: I1b6d1e3b7cdda15fd7a8f8f43d14ae6865bc3512

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -122,7 +122,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/charger.fstab.qcom
+LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
