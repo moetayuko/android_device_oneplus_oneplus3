b6450bbdad15bb550d4789f841fe25199b017a9c
dianlujitao <dianlujitao@gmail.com>
2016-06-18 23:43:53 +0800

    oneplus3: Fix touch keys, clean up keylayout
    Change-Id: I865bab2800fa6678093ed7657688c8324a552f95

--- device.mk
+++ device.mk
@@ -187,8 +187,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/fpc1020.kl:system/usr/keylayout/fpc1020.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl \
-    $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
-    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
+    $(LOCAL_PATH)/keylayout/synaptics.kl:system/usr/keylayout/synaptics.kl
 
 # Lights
 PRODUCT_PACKAGES += \
