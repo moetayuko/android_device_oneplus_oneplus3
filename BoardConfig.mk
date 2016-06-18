74155aa0b66e472df98f08c8078b3f72b344c191
dianlujitao <dianlujitao@gmail.com>
2016-06-19 01:15:03 +0800

    oneplus3: Support for native tap-to-wake
    Change-Id: I14aad134955a62fc6fd16a36618112e73922b229

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,6 +102,7 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # CM Hardware
 BOARD_HARDWARE_CLASS := \
     $(DEVICE_PATH)/cmhw
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
