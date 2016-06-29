f8ef2f171802e59fc58b513f14db8102c9ceac2a
dianlujitao <dianlujitao@gmail.com>
2016-06-29 23:17:07 +0800

    oneplus3: Fix decryption
    Change-Id: Iaff0c3daca775636b43960c78fda13d3f70a9715

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
