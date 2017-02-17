bf25de199b13693b94329b2275dd97233141ef03
Vinay Verma <vvinay@codeaurora.org>
2017-02-17 17:33:34 +0530

    Enable keymster HAL compilation
    Enable impl HAL compilation for keymaster.
    
    Change-Id: I65d0b85b0263ccb02d95bdc6532f5a1b8545fde4

--- msm8996.mk
+++ msm8996.mk
@@ -164,6 +164,8 @@ ifneq (,$(strip $(wildcard $(PRODUCT_RENDERING_ENGINE_REVLIB))))
         MULTI_LANG_ENGINE := REVERIE
 endif
 
+#Enable keymaster Impl HAL Compilation
+PRODUCT_PACKAGES += android.hardware.keymaster@3.0-impl
 # Defined the locales
 PRODUCT_LOCALES += th_TH vi_VN tl_PH hi_IN ar_EG ru_RU tr_TR pt_BR bn_IN mr_IN ta_IN te_IN zh_HK \
         in_ID my_MM km_KH sw_KE uk_UA pl_PL sr_RS sl_SI fa_IR kn_IN ml_IN ur_IN gu_IN or_IN
