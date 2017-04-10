ab62578a0cbc2c3683603be885714262c9ff84d9
Bharat Pawar <bpawar@codeaurora.org>
2017-04-10 20:46:09 +0530

    vintf: Update display hals
    * Add configstore, memtrack and lights
    * Default allocator and composer to hwbinder
    
    Change-Id: I32846edaa520bdb1efcadb2edb470bedfd989b94

--- msm8996.mk
+++ msm8996.mk
@@ -122,16 +122,19 @@ PRODUCT_PACKAGES += \
     antradio_app \
     libvolumelistener
 
-# Gralloc
+# Display/Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl
-
-# HW Composer
-PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
+    android.hardware.configstore@1.0-service
+
 
 # Sensor HAL conf file
 PRODUCT_COPY_FILES += \
