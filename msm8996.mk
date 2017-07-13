f412b5d5de1e366211be975fdd8ed1789ded8653
Paras Nagda <pnagda@codeaurora.org>
2017-07-13 12:18:16 +0530

    msm8996: Pick vendor MediaProfiles.xml
    Adding vendor media_profiles.xml, adding
    property to pick the same, while parsing
    profiles.xml
    
    Change-Id: I2a5eef05bf8cf4895dbaf7d06929347c564d8c23

--- msm8996.mk
+++ msm8996.mk
@@ -46,7 +46,7 @@ TARGET_USES_MEDIA_EXTENSIONS := true
 ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
 PRODUCT_COPY_FILES += \
     device/qcom/msm8996/media_profiles.xml:system/etc/media_profiles.xml \
-    device/qcom/msm8996/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
+    device/qcom/msm8996/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
     device/qcom/msm8996/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     device/qcom/msm8996/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
 endif  #TARGET_ENABLE_QC_AV_ENHANCEMENTS
