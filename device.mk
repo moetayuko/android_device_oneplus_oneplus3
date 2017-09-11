bd4183e1a97376cf39fb8bf9b5461372048586a7
dianlujitao <dianlujitao@lineageos.org>
2017-09-11 18:05:19 +0800

    HAX: op3: Disable unsupported packages
    Change-Id: I93fb2002d4c0e6360f713597d10c42f0df745f71

--- device.mk
+++ device.mk
@@ -212,10 +212,6 @@ PRODUCT_PACKAGES += \
     qdcm_calib_data_samsung_s6e3fa3_1080p_cmd_mode_dsi_panel.xml \
     qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    OneplusDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
@@ -225,7 +221,6 @@ PRODUCT_PACKAGES += \
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
     fingerprintd \
-    OneplusPocketMode \
     android.hardware.biometrics.fingerprint@2.1-service
 
 # Gatekeeper HAL
