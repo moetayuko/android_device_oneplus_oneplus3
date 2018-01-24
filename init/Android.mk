94923a16a6387eaced38cf4323987b43c9d23412
dianlujitao <dianlujitao@lineageos.org>
2018-01-24 18:06:28 +0800

    op3: Update for 8.1
     * Update tree to build against omni 8.1
     * Update QSEE blobs from stock 8.0
     * Remove unused QSEE blobs
     * Ship LA.UM.6.5.r1-05300-8x96.0 based prebuilt kernel

--- init/Android.mk
+++ init/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The Android Open Source Project
+# Copyright (C) 2017-2018 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,7 +19,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
+LOCAL_C_INCLUDES := \
+    system/core/base/include \
+    system/core/init
 LOCAL_SRC_FILES := init_oneplus3.cpp
 LOCAL_MODULE := libinit_oneplus3
 
--- init/init_oneplus3.cpp
+++ init/init_oneplus3.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2017 The Android Open Source Project
+   Copyright (C) 2017-2018 The Android Open Source Project
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -30,68 +30,20 @@
 #include <cstdlib>
 #include <unistd.h>
 #include <fcntl.h>
+#include <android-base/logging.h>
+#include <android-base/properties.h>
 
-#include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
-#include "util.h"
 
-static int read_file2(const char *fname, char *data, int max_size)
-{
-    int fd, rc;
-
-    if (max_size < 1)
-        return 0;
-
-    fd = open(fname, O_RDONLY);
-    if (fd < 0) {
-        ERROR("failed to open '%s'\n", fname);
-        return 0;
-    }
-
-    rc = read(fd, data, max_size - 1);
-    if ((rc > 0) && (rc < max_size))
-        data[rc] = '\0';
-    else
-        data[0] = '\0';
-    close(fd);
-
-    return 1;
-}
-
-void init_alarm_boot_properties()
-{
-    char const *alarm_file = "/proc/sys/kernel/boot_reason";
-    char buf[64];
-
-    if(read_file2(alarm_file, buf, sizeof(buf))) {
-
-    /*
-     * Setup ro.alarm_boot value to true when it is RTC triggered boot up
-     * For existing PMIC chips, the following mapping applies
-     * for the value of boot_reason:
-     *
-     * 0 -> unknown
-     * 1 -> hard reset
-     * 2 -> sudden momentary power loss (SMPL)
-     * 3 -> real time clock (RTC)
-     * 4 -> DC charger inserted
-     * 5 -> USB charger insertd
-     * 6 -> PON1 pin toggled (for secondary PMICs)
-     * 7 -> CBLPWR_N pin toggled (for external power supply)
-     * 8 -> KPDPWR_N pin toggled (power key pressed)
-     */
-        if(buf[0] == '3')
-            property_set("ro.alarm_boot", "true");
-        else
-            property_set("ro.alarm_boot", "false");
-    }
-}
+namespace android {
+namespace init {
 
 void load_op3(const char *model) {
     property_set("ro.product.model", model);
     property_set("ro.build.product", "OnePlus3");
     property_set("ro.product.device", "OnePlus3");
+    property_set("ro.vendor.product.device", "OnePlus3");
     property_set("ro.display.series", "OnePlus 3");
 }
 
@@ -99,11 +51,12 @@ void load_op3t(const char *model) {
     property_set("ro.product.model", model);
     property_set("ro.build.product", "OnePlus3");
     property_set("ro.product.device", "OnePlus3T");
+    property_set("ro.vendor.product.device", "OnePlus3T");
     property_set("ro.display.series", "OnePlus 3T");
 }
 
 void vendor_load_properties() {
-    int rf_version = stoi(property_get("ro.boot.rf_version"));
+    int rf_version = stoi(android::base::GetProperty("ro.boot.rf_version", ""));
 
     switch (rf_version) {
     case 11:
@@ -128,8 +81,9 @@ void vendor_load_properties() {
         load_op3t("ONEPLUS A3000");
         break;
     default:
-        INFO("%s: unexcepted rf version!\n", __func__);
+        LOG(ERROR) << __func__ << ": unexcepted rf version!";
     }
-
-    init_alarm_boot_properties();
 }
+
+}  // namespace init
+}  // namespace android
